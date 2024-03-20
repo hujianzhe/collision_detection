96be9fb5c9338b1400392de93a9a9cab0896b68b
zzz <zzz@taomee-ex.com>
2024-03-20 15:48:11 +0800

    support CCTNum_t long double

--- inc/number_define.h
+++ inc/number_define.h
@@ -41,8 +41,25 @@
 	#define	CCTNum_sin(n)			sin(n)
 	#define	CCTNum_atan2(y, x)		atan2(y, x)
 
+#elif	CCT_NUM_LONG_DOUBLE
+	typedef	long double				CCTNum_t;
+	#define	CCTNum(n)				n##L
+	#define	CCTNums_3(x, y, z)		x##L, y##L, z##L
+	#define	CCTNums_4(x, y, z, w)	x##L, y##L, z##L, w##L
+
+	#define	CCT_EPSILON				1e-5L
+	#define	CCT_EPSILON_NEGATE		-1e-5L
+	#define	CCT_GAP_DISTANCE		1e-3L
+
+	#define	CCTNum_abs(n)			fabsl(n)
+	#define	CCTNum_sqrt(n)			sqrtl(n)
+	#define	CCTNum_acos(n)			acosl(n)
+	#define	CCTNum_cos(n)			cosl(n)
+	#define	CCTNum_sin(n)			sinl(n)
+	#define	CCTNum_atan2(y, x)		atan2l(y, x)
+
 #else
-	#error	"CCTNum_t type isn't defined, should be float or double"
+	#error	"CCTNum_t type isn't defined, should be float/double/long double ."
 #endif
 
 #define	CCTNum_try_fix_to(n, to)	((CCTNum_abs(n - to) > CCT_EPSILON ? n : to))
