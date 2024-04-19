10d3661d32ee468a95829b6bc0aac0eb51fd2178
hujianzhe <976784480@qq.com>
2024-04-19 22:15:39 +0800

    add note

--- inc/math_matrix3.h
+++ inc/math_matrix3.h
@@ -8,7 +8,14 @@
 #include "compiler_define.h"
 #include "number_define.h"
 
-/* note: Matrix[column][row] */
+/* 
+ * note: Matrix[column][row]
+ et. m[16] layout
+ |m[0]	m[4]	m[8]	m[12]|
+ |m[1]	m[5]	m[9]	m[13]|
+ |m[2]	m[6]	m[10]	m[14]|
+ |m[3]	m[7]	m[11]	m[15]|
+ */
 
 #ifdef	__cplusplus
 extern "C" {
