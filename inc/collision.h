74bdfaa069d5215a0ec21a5d434d1998b5a3f28f
hujianzhe <976784480@qq.com>
2024-05-20 23:55:27 +0800

    rigid body

--- inc/collision.h
+++ inc/collision.h
@@ -16,6 +16,12 @@ typedef struct CCTResult_t {
 	CCTNum_t unique_hit_point[3];
 } CCTResult_t;
 
+typedef struct CCTRigidBody_t {
+	CCTNum_t velocity[3];
+	CCTNum_t mass;
+	CCTNum_t cor;
+} CCTRigidBody_t;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -23,6 +29,9 @@ extern "C" {
 __declspec_dll int mathCollisionContain(const GeometryBodyRef_t* one, const GeometryBodyRef_t* two);
 __declspec_dll int mathCollisionIntersect(const GeometryBodyRef_t* one, const GeometryBodyRef_t* two);
 __declspec_dll CCTResult_t* mathCollisionSweep(const GeometryBodyRef_t* one, const CCTNum_t dir[3], const GeometryBodyRef_t* two, CCTResult_t* result);
+
+__declspec_dll CCTRigidBody_t* physRigidBodyInit(CCTRigidBody_t* rigid);
+__declspec_dll void physCollisionResolveVelocity(const CCTRigidBody_t* src_rd, const CCTRigidBody_t* target_rd, const CCTNum_t hit_normal[3], CCTNum_t src_v[3], CCTNum_t target_v[3]);
 
 #ifdef	__cplusplus
 }
---
+++ src/collision_resolve.c
@@ -0,0 +1,66 @@
+//
+// Created by hujianzhe
+//
+
+#include "../inc/math_vec3.h"
+#include "../inc/collision.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+CCTRigidBody_t* physRigidBodyInit(CCTRigidBody_t* rigid) {
+	mathVec3Set(rigid->velocity, CCTNums_3(0.0, 0.0, 0.0));
+	rigid->mass = CCTNum(0.0);
+	rigid->cor = CCTNum(1.0);
+	return rigid;
+}
+
+void physCollisionResolveVelocity(const CCTRigidBody_t* src_rd, const CCTRigidBody_t* target_rd, const CCTNum_t hit_normal[3], CCTNum_t src_v[3], CCTNum_t target_v[3]) {
+	CCTNum_t dot;
+	CCTNum_t total_mass = src_rd->mass + target_rd->mass;
+	if (mathVec3IsZero(target_rd->velocity)) {
+		mathVec3Reflect(src_v, src_rd->velocity, hit_normal);
+		mathVec3Copy(target_v, hit_normal);
+		dot = mathVec3Dot(src_v, target_v);
+		if (dot > CCTNum(0.0)) {
+			mathVec3Negate(target_v, target_v);
+		}
+		mathVec3MultiplyScalar(src_v, src_v, src_rd->cor);
+		mathVec3MultiplyScalar(target_v, target_v, target_rd->cor);
+	}
+	else {
+		CCTNum_t delta_v[3];
+		mathVec3Sub(delta_v, src_rd->velocity, target_rd->velocity);
+		dot = mathVec3Dot(delta_v, hit_normal);
+		if (src_rd->cor < target_rd->cor) {
+			dot *= src_rd->cor;
+		}
+		else {
+			dot *= target_rd->cor;
+		}
+		if (total_mass > CCT_EPSILON) {
+			CCTNum_t impulse = CCTNum(2.0) * dot / total_mass;
+			CCTNum_t src_impluse = impulse, target_impluse = impulse;
+			if (target_rd->mass > CCT_EPSILON) {
+				src_impluse *= target_rd->mass;
+			}
+			mathVec3MultiplyScalar(delta_v, hit_normal, src_impluse);
+			mathVec3Sub(src_v, src_rd->velocity, delta_v);
+			if (src_rd->mass > CCT_EPSILON) {
+				target_impluse *= src_rd->mass;
+			}
+			mathVec3MultiplyScalar(delta_v, hit_normal, target_impluse);
+			mathVec3Add(target_v, target_rd->velocity, delta_v);
+		}
+		else {
+			mathVec3MultiplyScalar(delta_v, hit_normal, dot);
+			mathVec3Sub(src_v, src_rd->velocity, delta_v);
+			mathVec3Add(target_v, target_rd->velocity, delta_v);
+		}
+	}
+}
+
+#ifdef	__cplusplus
+}
+#endif
