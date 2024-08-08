609873c39922e5d2e8d2c012885705797ee48be0
zzz <zzz@taomee-ex.com>
2024-08-08 11:54:13 +0800

    remove useless code

--- inc/line_segment.h
+++ inc/line_segment.h
@@ -7,28 +7,17 @@
 
 #include "geometry_def.h"
 
-enum {
-	GEOMETRY_LINE_SKEW = 0,
-	GEOMETRY_LINE_PARALLEL = 1,
-	GEOMETRY_LINE_CROSS = 2,
-	GEOMETRY_LINE_OVERLAP = 3,
-
-	GEOMETRY_SEGMENT_CONTACT = 1,
-	GEOMETRY_SEGMENT_OVERLAP = 2,
-};
-
 #ifdef	__cplusplus
 extern "C" {
 #endif
 
 __declspec_dll CCTNum_t mathPointProjectionLine(const CCTNum_t p[3], const CCTNum_t ls_v[3], const CCTNum_t lsdir[3], CCTNum_t np[3]);
-__declspec_dll int mathLineClosestLine(const CCTNum_t lsv1[3], const CCTNum_t lsdir1[3], const CCTNum_t lsv2[3], const CCTNum_t lsdir2[3], CCTNum_t* min_d, CCTNum_t dir_d[2]);
+__declspec_dll void mathLineClosestLine(const CCTNum_t lsv1[3], const CCTNum_t lsdir1[3], const CCTNum_t lsv2[3], const CCTNum_t lsdir2[3], CCTNum_t* min_d, CCTNum_t dir_d[2]);
 
 __declspec_dll int mathSegmentIsSame(const CCTNum_t ls1[2][3], const CCTNum_t ls2[2][3]);
 __declspec_dll void mathSegmentClosestPointTo(const CCTNum_t ls[2][3], const CCTNum_t p[3], CCTNum_t closest_p[3]);
 __declspec_dll void mathSegmentClosestPointTo_v2(const CCTNum_t ls_center_p[3], const CCTNum_t lsdir[3], const CCTNum_t ls_half_len, const CCTNum_t p[3], CCTNum_t closest_p[3]);
 __declspec_dll CCTNum_t mathSegmentSegmentClosestIndices(const CCTNum_t ls1[2][3], const CCTNum_t ls2[2][3], unsigned int* ls1_indices, unsigned int* ls2_indices);
-__declspec_dll int mathSegmentClosestSegment(const CCTNum_t ls1[2][3], const CCTNum_t ls2[2][3], CCTNum_t closest_p[2][3]);
 
 #ifdef	__cplusplus
 }
--- src/line_segment.c
+++ src/line_segment.c
@@ -21,7 +21,7 @@ CCTNum_t mathPointProjectionLine(const CCTNum_t p[3], const CCTNum_t ls_v[3], co
 	return dot;
 }
 
-int mathLineClosestLine(const CCTNum_t lsv1[3], const CCTNum_t lsdir1[3], const CCTNum_t lsv2[3], const CCTNum_t lsdir2[3], CCTNum_t* min_d, CCTNum_t dir_d[2]) {
+void mathLineClosestLine(const CCTNum_t lsv1[3], const CCTNum_t lsdir1[3], const CCTNum_t lsv2[3], const CCTNum_t lsdir2[3], CCTNum_t* min_d, CCTNum_t dir_d[2]) {
 	CCTNum_t N[3], n[3], v[3], dot, nlen;
 	mathVec3Sub(v, lsv2, lsv1);
 	mathVec3Cross(n, lsdir1, lsdir2);
@@ -33,12 +33,12 @@ int mathLineClosestLine(const CCTNum_t lsv1[3], const CCTNum_t lsdir1[3], const 
 			if (min_d) {
 				*min_d = CCTNum_sqrt(nlen - dot);
 			}
-			return GEOMETRY_LINE_PARALLEL;
+			return;
 		}
 		if (min_d) {
 			*min_d = CCTNum(0.0);
 		}
-		return GEOMETRY_LINE_OVERLAP;
+		return;
 	}
 	if (mathVec3IsZero(v)) {
 		if (min_d) {
@@ -47,7 +47,7 @@ int mathLineClosestLine(const CCTNum_t lsv1[3], const CCTNum_t lsdir1[3], const 
 		if (dir_d) {
 			dir_d[0] = dir_d[1] = CCTNum(0.0);
 		}
-		return GEOMETRY_LINE_CROSS;
+		return;
 	}
 	nlen = mathVec3Normalized(N, n);
 	dot = mathVec3Dot(v, N);
@@ -59,74 +59,6 @@ int mathLineClosestLine(const CCTNum_t lsv1[3], const CCTNum_t lsdir1[3], const 
 		CCTNum_t cross_v[3], nlensq_inv = CCTNum(1.0) / CCTNum_sq(nlen);
 		dir_d[0] = mathVec3Dot(mathVec3Cross(cross_v, v, lsdir2), n) * nlensq_inv;
 		dir_d[1] = mathVec3Dot(mathVec3Cross(cross_v, v, lsdir1), n) * nlensq_inv;
-	}
-	return dot > CCT_EPSILON ? GEOMETRY_LINE_SKEW : GEOMETRY_LINE_CROSS;
-}
-
-static int mathSegmentIntersectSegmentWhenInSameLine(const CCTNum_t ls1[2][3], const CCTNum_t ls2[2][3], CCTNum_t p[3]) {
-	CCTNum_t dot, lsdir1[3], lsdir2[3];
-	mathVec3Sub(lsdir1, ls1[1], ls1[0]);
-	mathVec3Sub(lsdir2, ls2[1], ls2[0]);
-	if (mathVec3Equal(ls1[0], ls2[0])) {
-		dot = mathVec3Dot(lsdir2, lsdir1);
-		if (dot <= CCT_EPSILON) {
-			if (p) {
-				mathVec3Copy(p, ls1[0]);
-			}
-			return GEOMETRY_SEGMENT_CONTACT;
-		}
-		return GEOMETRY_SEGMENT_OVERLAP;
-	}
-	else if (mathVec3Equal(ls1[0], ls2[1])) {
-		dot = mathVec3Dot(lsdir2, lsdir1);
-		if (dot >= CCT_EPSILON) {
-			if (p) {
-				mathVec3Copy(p, ls1[0]);
-			}
-			return GEOMETRY_SEGMENT_CONTACT;
-		}
-		return GEOMETRY_SEGMENT_OVERLAP;
-	}
-	else if (mathVec3Equal(ls1[1], ls2[0])) {
-		dot = mathVec3Dot(lsdir2, lsdir1);
-		if (dot >= CCT_EPSILON) {
-			if (p) {
-				mathVec3Copy(p, ls1[1]);
-			}
-			return GEOMETRY_SEGMENT_CONTACT;
-		}
-		return GEOMETRY_SEGMENT_OVERLAP;
-	}
-	else if (mathVec3Equal(ls1[1], ls2[1])) {
-		dot = mathVec3Dot(lsdir2, lsdir1);
-		if (dot <= CCT_EPSILON) {
-			if (p) {
-				mathVec3Copy(p, ls1[1]);
-			}
-			return GEOMETRY_SEGMENT_CONTACT;
-		}
-		return GEOMETRY_SEGMENT_OVERLAP;
-	}
-	else {
-		CCTNum_t v1[3], v2[3];
-		int i;
-		for (i = 0; i < 2; ++i) {
-			mathVec3Sub(v1, ls1[0], ls2[i]);
-			mathVec3Sub(v2, ls1[1], ls2[i]);
-			dot = mathVec3Dot(v1, v2);
-			if (dot < CCT_EPSILON_NEGATE) {
-				return GEOMETRY_SEGMENT_OVERLAP;
-			}
-		}
-		for (i = 0; i < 2; ++i) {
-			mathVec3Sub(v1, ls2[0], ls1[i]);
-			mathVec3Sub(v2, ls2[1], ls1[i]);
-			dot = mathVec3Dot(v1, v2);
-			if (dot < CCT_EPSILON_NEGATE) {
-				return GEOMETRY_SEGMENT_OVERLAP;
-			}
-		}
-		return 0;
 	}
 }
 
@@ -163,99 +95,6 @@ CCTNum_t mathSegmentSegmentClosestIndices(const CCTNum_t ls1[2][3], const CCTNum
 	}
 
 	return min_lensq;
-}
-
-int mathSegmentClosestSegment(const CCTNum_t ls1[2][3], const CCTNum_t ls2[2][3], CCTNum_t closest_p[2][3]) {
-	CCTNum_t dir1[3], dir2[3], lslen1, lslen2, dir_d[2];
-	int res, i, has_p;
-	unsigned int closest_ls1_indice, closest_ls2_indice;
-	mathVec3Sub(dir1, ls1[1], ls1[0]);
-	mathVec3Sub(dir2, ls2[1], ls2[0]);
-	lslen1 = mathVec3Normalized(dir1, dir1);
-	lslen2 = mathVec3Normalized(dir2, dir2);
-	res = mathLineClosestLine(ls1[0], dir1, ls2[0], dir2, NULL, dir_d);
-	if (GEOMETRY_LINE_PARALLEL == res) {
-		for (i = 0; i < 2; ++i) {
-			CCTNum_t v[3], dot;
-			mathVec3Sub(v, ls1[i], ls2[0]);
-			dot = mathVec3Dot(v, dir2);
-			if (dot >= CCT_EPSILON_NEGATE && dot <= lslen2 + CCT_EPSILON) {
-				mathVec3Copy(closest_p[0], ls1[i]);
-				mathVec3Copy(closest_p[1], ls2[0]);
-				mathVec3AddScalar(closest_p[1], dir2, dot);
-				return 0;
-			}
-		}
-	}
-	else if (GEOMETRY_LINE_CROSS == res || GEOMETRY_LINE_SKEW == res) {
-		if (dir_d[0] >= CCT_EPSILON && dir_d[0] <= lslen1 + CCT_EPSILON &&
-			dir_d[1] >= CCT_EPSILON && dir_d[1] <= lslen2 + CCT_EPSILON)
-		{
-			mathVec3Copy(closest_p[0], ls1[0]);
-			mathVec3AddScalar(closest_p[0], dir1, dir_d[0]);
-			mathVec3Copy(closest_p[1], ls2[0]);
-			mathVec3AddScalar(closest_p[1], dir2, dir_d[1]);
-			return GEOMETRY_LINE_CROSS == res ? GEOMETRY_SEGMENT_CONTACT : 0;
-		}
-		has_p = 0;
-		for (i = 0; i < 2; ++i) {
-			CCTNum_t v[3], dot;
-			mathVec3Sub(v, ls1[i], ls2[0]);
-			dot = mathVec3Dot(v, dir2);
-			if (dot < CCT_EPSILON_NEGATE || dot > lslen2 + CCT_EPSILON) {
-				continue;
-			}
-			if (has_p) {
-				CCTNum_t dq = mathVec3LenSq(v) - CCTNum_sq(dot);
-				mathVec3Sub(v, closest_p[1], closest_p[0]);
-				if (dq >= mathVec3LenSq(v)) {
-					continue;
-				}
-			}
-			has_p = 1;
-			mathVec3Copy(closest_p[0], ls1[i]);
-			mathVec3Copy(closest_p[1], ls2[0]);
-			mathVec3AddScalar(closest_p[1], dir2, dot);
-		}
-		for (i = 0; i < 2; ++i) {
-			CCTNum_t v[3], dot;
-			mathVec3Sub(v, ls2[i], ls1[0]);
-			dot = mathVec3Dot(v, dir1);
-			if (dot < CCT_EPSILON_NEGATE || dot > lslen1 + CCT_EPSILON) {
-				continue;
-			}
-			if (has_p) {
-				CCTNum_t dq = mathVec3LenSq(v) - CCTNum_sq(dot);
-				mathVec3Sub(v, closest_p[1], closest_p[0]);
-				if (dq >= mathVec3LenSq(v)) {
-					continue;
-				}
-			}
-			has_p = 1;
-			mathVec3Copy(closest_p[1], ls2[i]);
-			mathVec3Copy(closest_p[0], ls1[0]);
-			mathVec3AddScalar(closest_p[0], dir1, dot);
-		}
-		if (has_p) {
-			return 0;
-		}
-	}
-	else {
-		CCTNum_t p[3];
-		res = mathSegmentIntersectSegmentWhenInSameLine(ls1, ls2, p);
-		if (GEOMETRY_SEGMENT_CONTACT == res) {
-			mathVec3Copy(closest_p[0], p);
-			mathVec3Copy(closest_p[1], p);
-			return res;
-		}
-		if (GEOMETRY_SEGMENT_OVERLAP == res) {
-			return res;
-		}
-	}
-	mathSegmentSegmentClosestIndices(ls1, ls2, &closest_ls1_indice, &closest_ls2_indice);
-	mathVec3Copy(closest_p[0], ls1[closest_ls1_indice]);
-	mathVec3Copy(closest_p[1], ls2[closest_ls2_indice]);
-	return 0;
 }
 
 int mathSegmentIsSame(const CCTNum_t ls1[2][3], const CCTNum_t ls2[2][3]) {
